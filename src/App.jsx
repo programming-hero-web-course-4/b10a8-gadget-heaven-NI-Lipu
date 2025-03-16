ff629d39dd4c1c5b2ce602d06cbde9974fb9fbb4
NI Lipu <nilipu500@gmail.com>
2025-03-16 21:30:52 +0600

    Basic Setup react router project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+    <h1 className='font-bold '>Vite + React</h1>
+    <button className='btn btn-secondary'>ddddd</button>
+    </>
+      
+      
+      
+  )
+}
+
+export default App
